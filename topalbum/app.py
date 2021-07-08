95b662a4cb957c9bf89fc1d31e72b2e76de98599
George McCain <georgemccain88@gmail.com>
2021-07-08 17:49:40 -0400

    added functionality

--- topalbum/app.py
+++ topalbum/app.py
@@ -16,11 +16,35 @@ app = Flask(__name__)
 def home():
     album_information = requests.get(
         "https://api.spotify.com/v1/albums",
-        params={"ids": "4aawyAB9vmqN3uQ7FjRGTy,0sNOF9WDwhWunNAHPD3Baj"},
+        params={"ids": "3XLn9BkDkwq4icsUye2Krp,1oNWum7uuaISS4cSgDQhWT,2If8WvHmIANFsGWDf36e2M,2vmjRfAgNyYIiFsHuUU8u9"},
         headers={"Authorization": SPOTIFY_AUTHORIZATION},
     ).json()
+
     print(album_information)
     albums = album_information["albums"]
+
+    i = random.randint(0, 3)
+    j=random.randint(0,3)
+    
+    while j == i:
+        j = random.randint(0, 3)
+
+    album1 = album_information["albums"][i]
+    album2 = album_information["albums"][j]
+
+    albumdate1 = album1["release_date"]
+    albumdate2 = album2["release_date"]
+
+    name1 = album1["name"]
+    name2 = album2["name"]
+
+    popularity1 = album1["popularity"]
+    popularity2 = album2["popularity"]
+
+    image1 = album1["images"][0]["url"]
+    image2 = album2["images"][0]["url"]
+
+    
 
     #   artistname = album["artists"][0]["name"]
     #     albumname = album["name"]
@@ -33,7 +57,7 @@ def home():
     n = random.randint(0,22)
     
 
-    return render_template("index.html", albums=albums)
+    return render_template("index.html", album1=album1, album2=album2, image1=image1, image2=image2, name1=name1, name2=name2, popularity1=popularity1, popularity2=popularity2)
 
 
 
