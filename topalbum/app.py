ed4d0828bb7117636755cfc7ab0771b45661b729
George McCain <georgemccain88@gmail.com>
2021-07-11 14:15:06 -0400

    added more albums

--- topalbum/app.py
+++ topalbum/app.py
@@ -14,7 +14,7 @@ app = Flask(__name__)
 album_information = requests.get(
     "https://api.spotify.com/v1/albums",
     params={
-        "ids": "2vmjRfAgNyYIiFsHuUU8u9,1ATL5GLyefJaxhQzSPVrLX,0S0KGZnfBGSIssfF54WSJh,5zi7WsKlIiUXv09tbGLKsE,3arNdjotCvtiiLFfjKngMc,392p3shh2jkxUxY2VHvlH8,0ptlfJfwGTy0Yvrk14JK1I,6pwuKxMUkNg673KETsXPUV,41GuZcammIkupMPKH2OJ6I"
+        "ids": "2vmjRfAgNyYIiFsHuUU8u9,1ATL5GLyefJaxhQzSPVrLX,0S0KGZnfBGSIssfF54WSJh,5zi7WsKlIiUXv09tbGLKsE,3arNdjotCvtiiLFfjKngMc,392p3shh2jkxUxY2VHvlH8,0ptlfJfwGTy0Yvrk14JK1I,6pwuKxMUkNg673KETsXPUV,41GuZcammIkupMPKH2OJ6I,7gsWAHLeT0w7es6FofOXk1,0FgZKfoU2Br5sHOfvZKTI9,6b7ycwe2rxq6FkaupNuGoS,6GphKx2QAPRoVGWE9D7ou8,3PRoXYsngSwjEQWR5PsHWR,6YabPKtZAjxwyWbuO9p4ZD,50o7kf2wLwVmOTVYJOTplm,2v6ANhWhZBUKkg6pJJBs3B,5U4dnRZsfW8NmwBBkELFPh,6FCzvataOZh68j8OKzOt9a,4NP1rhnsPdYpnyJP0p0k0L"
     },
     headers={"Authorization": SPOTIFY_AUTHORIZATION},
 ).json()
